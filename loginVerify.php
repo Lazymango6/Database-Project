cf093027d82ebcd4ca591f2f2b67ec18736a3d55
Bijay Khanal <bijaya2626@gmail.com>
2025-03-27 11:24:06 -0500

    folder fix

---
+++ loginVerify.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'db_connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['role'] = $user['role'];
+
+        if ($user['role'] == 'manager') {
+            header("Location: manager_dashboard.php");
+        } else {
+            header("Location: employee_dashboard.php");
+        }
+        exit();
+    } else {
+        echo "Invalid credentials.";
+    }
+}
+?>
