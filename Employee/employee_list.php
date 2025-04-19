e95d5db0084ce960585325f447dd6b7d0477baca
jayTrivedi2 <jaytrivedi1339@gmail.com>
2025-04-18 19:38:14 -0500

    employee list v1

---
+++ Employee/employee_list.php
@@ -0,0 +1,12 @@
+<form action="employee_list.php" method="POST">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="department">Department:</label>
+    <input type="text" id="department" name="department" required>
+
+    <label for="position">Position:</label>
+    <input type="text" id="position" name="position" required>
+
+    <button type="submit">Add Employee</button>
+</form>
